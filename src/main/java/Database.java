7fcd63da04cff4a5148d510eb0a0e1c6b9862e7f
Seth-Graham <sethtgraham92@gmail.com>
2021-03-08 22:08:52 -0500

    Error Handling
    03/08/2021
    More fixes to the GUI

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -4,24 +4,24 @@ import java.sql.SQLException;
 
 public class Database {
 
-  private static String dbFileLocation = "jdbc:sqlite:src/main/resources/HealthConnectDB.db";
-  private static String username = "";
-  private static String password = "";
-  public static Connection connection = null;
+    private static String dbFileLocation = "jdbc:sqlite:src/main/resources/HealthConnectDB.db";
+    private static String username = "";
+    private static String password = "";
+    public static Connection connection = null;
 
-  public static void connectToDatabase() {
+    public static void connectToDatabase() {
 
-    try {
-      Class.forName("org.sqlite.JDBC");
-      connection = DriverManager.getConnection(dbFileLocation, username, password);
-      System.out.println("Connection to database has been established.");
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection(dbFileLocation, username, password);
+            System.out.println("Connection to database has been established.");
 
-    } catch (ClassNotFoundException e) {
-      e.printStackTrace();
-      System.out.println("Could not find the Driver.");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Could not find the Driver.");
 
-    } catch (SQLException se) {
-      se.printStackTrace();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
     }
-  }
 }
--- src/main/java/HealthConnectPractice_3.java
+++ src/main/java/HealthConnectPractice_3.java
@@ -5,40 +5,40 @@ import java.sql.SQLException;
 
 public class HealthConnectPractice_3 {
 
-  /**
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
-    // TODO code application logic here
-      //JOptionPane.showMessageDialog (null, "Connected");
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //JOptionPane.showMessageDialog (null, "Connected");
 
-    //Test the connection.
-    try {
+        //Test the connection.
+        try {
 
-      Database.connectToDatabase();
+            Database.connectToDatabase();
 
-      Statement statement = Database.connection.createStatement();
-      ResultSet hc = statement.executeQuery("select * from Patient");
+            Statement statement = Database.connection.createStatement();
+            ResultSet hc = statement.executeQuery("select * from Patient");
 
-      while (hc.next()) {
-        System.out.println("Username = " + hc.getString("Username"));
-        System.out.println("Password = " + hc.getString("Password"));
-      }
+            while (hc.next()) {
+                System.out.println("Username = " + hc.getString("Username"));
+                System.out.println("Password = " + hc.getString("Password"));
+            }
 
-      hc = statement.executeQuery("select * from Doctor");
+            hc = statement.executeQuery("select * from Doctor");
 
-      while (hc.next()) {
-        System.out.println("Username = " + hc.getString("Username"));
-        System.out.println("Password = " + hc.getString("Password"));
-      }
-      //open login page
-      NewJFrame s = new NewJFrame();
-      s.setVisible(true);
-    } catch (SQLException e) {
-      JOptionPane.showMessageDialog(null, e);
+            while (hc.next()) {
+                System.out.println("Username = " + hc.getString("Username"));
+                System.out.println("Password = " + hc.getString("Password"));
+            }
+            //open login page
+            NewJFrame s = new NewJFrame();
+            s.setVisible(true);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+
+
     }
-
-
-  }
 
 }
